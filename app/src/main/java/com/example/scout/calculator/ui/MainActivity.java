bf46e33161c668fe06adae8d0896a87ba74c80f5
Scott Holland <scotthollandjr@gmail.com>
2016-08-17 15:22:07 -0700

    Add functions for basic math. Add, subtract, divide, multiply, equals, clear

--- app/src/main/java/com/example/scout/calculator/ui/MainActivity.java
+++ app/src/main/java/com/example/scout/calculator/ui/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.scout.calculator.ui;
 
+import android.support.annotation.IntegerRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import com.example.scout.calculator.R;
+
+import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -30,6 +34,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Bind(R.id.buttonThree) TextView mButtonThree;
     @Bind(R.id.buttonTwo) TextView mButtonTwo;
     @Bind(R.id.buttonZero) TextView mButtonZero;
+    @Bind(R.id.equationText) TextView mEquationText;
+
+    private StringBuilder firstNum = new StringBuilder();
+    private StringBuilder secondNum = new StringBuilder();
+    private String firstNumString;
+    private String secondNumString;
+    private String mathObject;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,57 +67,108 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mButtonModulo.setOnClickListener(this);
         mButtonCase.setOnClickListener(this);
         mButtonClear.setOnClickListener(this);
+        mEquationText.setOnClickListener(this);
+
+        mEquationText.setText("0");
+        mathObject = "z";
+    }
+    
+    public void gatherEquation(String i) {
+        if (mathObject.equals("z")) {
+            buildFirstNum(i);
+        } else {
+            buildSecondNum(i);
+        }
+    }
+
+    public void buildFirstNum(String i) {
+        firstNum.append(i);
+    }
+
+    public void buildSecondNum(String i) {
+        secondNum.append(i);
+    }
+
+    public void clearStats() {
+        mathObject = "z";
+        firstNum.setLength(0);
+        secondNum.setLength(0);
+    }
+
+    public void processEquation() {
+        double result = 0.00;
+        int first = Integer.parseInt(firstNum.toString());
+        int second = Integer.parseInt(secondNum.toString());
+
+        if (mathObject.equals("add")) {
+            result = first + second;
+        } else if (mathObject.equals("sub")) {
+            result = first - second;
+        } else if (mathObject.equals("mul")) {
+            result = first * second;
+        } else if (mathObject.equals("div")) {
+            result = first / second;
+        } else if (mathObject.equals("z")){
+            result = first;
+        } else {
+
+        }
+
+        Log.d("MATH!", result + "");
+        mathObject = "z";
+        firstNum.setLength(0);
+        secondNum.setLength(0);
     }
 
     @Override
     public void onClick(View view) {
         if (view == mButtonOne) {
-
+            gatherEquation("1");
+        }
+        if (view == mButtonTwo) {
+            gatherEquation("2");
         }
         if (view == mButtonThree) {
-
-        }
-        if (view == mButtonThree) {
-
+            gatherEquation("3");
         }
         if (view == mButtonFour) {
-
+            gatherEquation("4");
         }
         if (view == mButtonFive) {
-
+            gatherEquation("5");
         }
         if (view == mButtonSix) {
-
+            gatherEquation("6");
         }
         if (view == mButtonSeven) {
-
+            gatherEquation("7");
         }
         if (view == mButtonEight) {
-
+            gatherEquation("8");
         }
         if (view == mButtonNine) {
-
+            gatherEquation("9");
         }
         if (view == mButtonZero) {
-
+            gatherEquation("0");
         }
         if (view == mButtonEquals) {
-
+            processEquation();
         }
         if (view == mButtonAdd) {
-
+            mathObject = "add";
         }
         if (view == mButtonSub) {
-
+            mathObject = "sub";
         }
         if (view == mButtonMult) {
-
+            mathObject = "mul";
         }
         if (view == mButtonDivide) {
-
+            mathObject = "div";
         }
         if (view == mButtonClear) {
-
+            clearStats();
         }
         if (view == mButtonCase) {
 
